91d0c6d4c067ee24685d978a0f8835983a8dc1ac
amansh45 <aman.asvickes.sharma45@gmail.com>
2020-05-28 12:41:58 +0530

    Background particle System

---
+++ Assets/Scripts/BackgroundParticles.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundParticles : MonoBehaviour {
+    
+    void Update()
+    {
+        transform.position = new Vector3(Camera.main.transform.position.x, Camera.main.transform.position.y, 0);
+    }
+}

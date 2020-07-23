2e739e1a58f77c1478019f2298e433388bef77e7
amansh45 <aman.asvickes.sharma45@gmail.com>
2020-07-23 09:49:00 +0530

    8 Levels

--- Assets/Scripts/EnemyLauncher.cs
+++ Assets/Scripts/EnemyLauncher.cs
@@ -4,31 +4,36 @@ using UnityEngine;
 
 public class EnemyLauncher : MonoBehaviour
 {
-
     [SerializeField] GameObject granadePrefab, playerPrefab;
-    [SerializeField] float distanceThreshold = 3f;
+    [SerializeField] float stillDistanceThreshold = 3f;
     [SerializeField] bool aimAtPlayer = false;
+    [SerializeField] bool isBlinking = false;
+    [SerializeField] float blinkDistanceThreshold = 0.5f;
 
     float initializationFactor = 1f, spawnScaleFactor = 0f;
     Granade latestGranadeClass;
     GameObject latestGranade;
     TaskHandler taskHandlerClass;
+    Animator anim;
     int counter = 0;
 
     private void Start()
     {
         taskHandlerClass = FindObjectOfType<TaskHandler>();
+        anim = GetComponent<Animator>();
+        if(isBlinking)
+            anim.enabled = false;
     }
 
     private void Update() {
 
-        if(playerPrefab != null && aimAtPlayer)
+        if (playerPrefab != null && aimAtPlayer)
         {
             Vector3 target = playerPrefab.transform.position;
 
             Vector3 objectPos = transform.position;
 
-            if (Vector3.Distance(target, objectPos) <= distanceThreshold && counter % 3 == 0)
+            if (Vector3.Distance(target, objectPos) <= stillDistanceThreshold && counter % 3 == 0)
             {
                 target.x = target.x - objectPos.x;
                 target.y = target.y - objectPos.y;
@@ -39,6 +44,18 @@ public class EnemyLauncher : MonoBehaviour
                     counter = 1;
             }
             counter += 1;
+        }
+        else if (playerPrefab != null && isBlinking)
+        {
+            float targetY = playerPrefab.transform.position.y;
+
+            float objectY = transform.position.y;
+
+            
+            if (Mathf.Abs(objectY - targetY) <= blinkDistanceThreshold)
+            {
+                anim.enabled = true;
+            }
         }
     }
 
@@ -53,6 +70,7 @@ public class EnemyLauncher : MonoBehaviour
     {
         latestGranade = Instantiate(granadePrefab, transform.position, transform.rotation) as GameObject;
         latestGranadeClass = latestGranade.GetComponent<Granade>();
+        Debug.Log("Granade Initiated....");
     }
 
     public void SetSlowmoForGranadeLauncher(float slowmoFactor)
--- Assets/Scripts/InitiateFall.cs
+++ Assets/Scripts/InitiateFall.cs
@@ -15,7 +15,7 @@ public class InitiateFall : MonoBehaviour
 
     void Update()
     {
-        if(Vector2.Distance(transform.position, playerPrefab.transform.position) <= thresholdDistance && !isFalling)
+        if (playerPrefab != null && Vector2.Distance(transform.position, playerPrefab.transform.position) <= thresholdDistance && !isFalling)
         {
             Debug.Log(Vector2.Distance(transform.position, playerPrefab.transform.position));
             gameObject.AddComponent<Rigidbody2D>();
